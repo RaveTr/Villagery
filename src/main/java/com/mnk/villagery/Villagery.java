0478f926488c84b1645fc16f8ea5b1975a56d437
RaveTr <yuyuss210@gmail.com>
2022-12-27 12:10:53 +0200

    Initial commit

---
+++ src/main/java/com/mnk/villagery/Villagery.java
@@ -0,0 +1,45 @@
+package com.mnk.villagery;
+
+import java.util.Optional;
+
+import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.forgespi.language.IModInfo;
+import software.bernie.geckolib.GeckoLib;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Villagery.MODID)
+public class Villagery {
+	public static final String MODNAME = "Villagery";
+    public static final String MODID = "villagery";
+    public static Villagery INSTANCE;
+	public static ArtifactVersion VERSION = null;
+    private static final Logger LOGGER = LogUtils.getLogger();
+    
+    public Villagery() {
+    	GeckoLib.initialize();
+    	INSTANCE = this;
+    	
+		Optional<? extends ModContainer> villageryMod = ModList.get().getModContainerById(MODID);
+		if (villageryMod.isPresent()) {
+			IModInfo modInfo = villageryMod.get().getModInfo();
+			VERSION = modInfo.getVersion();
+		} else {
+			LOGGER.warn("Couldn't get mod version from mod info..");
+		}
+    	
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus forgeBus =  MinecraftForge.EVENT_BUS;
+        
+        forgeBus.register(this);
+    }
+}
